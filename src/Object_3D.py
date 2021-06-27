a84399555c9cf34db2d955682375c6457595fc2e
Naruki-Ichihara <csna20005@g.nihon-u.ac.jp>
2021-06-27 01:05:31 +0000

    implimented generate_support_volume by voxel method

--- src/Object_3D.py
+++ src/Object_3D.py
@@ -1,41 +1,11 @@
 import numpy as np
 import trimesh as tr
-import matplotlib.pyplot as plt
 from trimesh.transformations import euler_matrix
 from vedo import *
 from tqdm import tqdm
-from scipy.spatial.qhull import Delaunay
-from trimesh import Trimesh, transformations
-from trimesh.geometry import plane_transform
-from mpl_toolkits.mplot3d import Axes3D
-
-def _remove_overlapping_triangles(m: Trimesh) -> Trimesh:
-    cleaned_mesh = Trimesh()
-    used_faces = []
-    face_indices = np.arange(0, m.faces.shape[0])
-
-    # For all facets reconstruct the surfaces using Delaunay triangulation
-    for facet_faces in m.facets:
-        if len(facet_faces):
-            used_faces.extend(facet_faces)
-            selected_faces = m.faces[facet_faces].flatten()
-            facet_vertices = m.vertices[selected_faces]
-            to_2d = plane_transform(facet_vertices[0], m.face_normals[facet_faces[0]])
-            vertices_2d = transformations.transform_points(
-                facet_vertices,
-                matrix=to_2d)[:, :2]
-            tri = Delaunay(vertices_2d)
-            split = Trimesh(vertices=facet_vertices, faces=tri.simplices)
-            cleaned_mesh += split
-
-    # Create a mask of faces that were not part of a facet
-    mask = np.ones(face_indices.size, dtype=bool)
-    mask[np.unique(used_faces)] = False
-
-    # Add those remaining faces back in
-    cleaned_mesh += m.submesh([face_indices[mask]], only_watertight=False)
-    cleaned_mesh.visual = m.visual
-    return cleaned_mesh
+import PVGeo as pg
+import pyvista as pv
+from scipy.optimize import fmin
 
 class Object_3D():
     def __init__(self, file):
@@ -43,6 +13,9 @@ class Object_3D():
         self.bounds = self.object.bounds
         self.volume = self.object.volume
         self.centroid = self.object.centroid
+
+    def is_watertight(self):
+        return self.object.is_watertight
 
     def tlanslate_to_origin(self, origin=[0, 0, 0]):
         translate = np.array(origin) - np.array(self.centroid)
@@ -88,44 +61,49 @@ class Object_3D():
         submesh = self.object.submesh([id], append=True)
         return submesh
 
-    def generate_support_volume(self, limit_angle=45.0, build_plane=None):
-        ''' TO DO '''
-        pitch = 1.5
+    def generate_support_volume(self, limit_angle=45.0, resolution=150, build_plane=None, export_as_vtk=False, ignore_overlap_region=True):
         if build_plane is None:
-            build_plane = Plane(pos=self.bounds[0, :], sx=1000, sy=1000)
-        m = self.overhang_checker(limit_angle)
-        #v = m.vertices
-        v,f = tr.remesh.subdivide_to_size(m.vertices, m.faces, pitch/2)
-        hit = (v / pitch)
-        hit = np.vstack((np.ceil(hit), np.floor(hit))).astype(int)
-        u,i = tr.grouping.unique_rows(hit)
-        occupieds = hit[u]*pitch
-        z = []
-        for occuped in occupieds:
-            z.append(occuped[2])
-        z = np.array(z)
-        ars = []
+            build_plane = Plane(pos=self.bounds[0, :], sx=1, sy=1)
+        overhang_submesh = self.overhang_checker(limit_angle)
+        length = overhang_submesh.bounds[1, 0] - overhang_submesh.bounds[0, 0]
+        pitch = length / resolution
+        vox = overhang_submesh.voxelized(pitch).points
+        hit = (vox / pitch)
+        hit = np.vstack((np.ceil(hit), np.floor(hit)))
+        u, i = tr.grouping.unique_rows(hit)
+        occupied = hit[u]*pitch
+        z = occupied[:, 2]
+        pts = []
         for i in range(len(z)):
-            ar_z = np.arange(np.min(z), z[i])
-            ar_x = np.full(ar_z.shape, occupieds[i, 0])
-            ar_y = np.full(ar_z.shape, occupieds[i, 1])
-            ar = np.stack([ar_x, ar_y, ar_z], 1)
-            ars.append(ar)
-        ar = np.concatenate(ars, 0)
+            pt_z = np.arange(np.min(z), z[i], pitch)
+            pt_x = np.full(pt_z.shape, occupied[i, 0])
+            pt_y = np.full(pt_z.shape, occupied[i, 1])
+            pt = np.stack([pt_x, pt_y, pt_z], 1)
+            pts.append(pt)
+        grid_ = np.concatenate(pts, 0)
+        u, i = tr.grouping.unique_rows(grid_)
+        grid = grid_[u]
 
-        cdt = self.object.contains(ar)
-        ar_ = ar[not cdt]
-        print(ar.shape[0]*pitch**3)
+        if not ignore_overlap_region:
+            vedo_volume = trimesh2vedo(self.object)
+            outward_pts = vedo_volume.insidePoints(grid, invert=True)
+            grid = outward_pts.points()
 
-        return ar_.shape[0]*pitch**3
+        if export_as_vtk:
+            vtkpoints = pg.points_to_poly_data(grid)
+            voxlizer = pg.filters.VoxelizePoints()
+            vox = voxlizer.apply(vtkpoints)
+            vox_vista = pv.wrap(vox)
+            vox_vista.save('support_volume.vtk')
+
+        return grid.shape[0]*pitch**3
 
 print('Process start')
 print('Loading mesh')
 obj = Object_3D('/workdir/models/bracket.stl')
 obj.tlanslate_to_origin()
-obj.apply_rotation_matrix((np.deg2rad(20), 0, 0))
+obj.apply_rotation_matrix((np.deg2rad(45), 0, 0))
 obj.apply_scale_matrix(0.5)
-print('Support')
+print('Support Volume estimate:')
 vol = obj.generate_support_volume()
-obj.object.export('mesh.stl')
 print(vol)
