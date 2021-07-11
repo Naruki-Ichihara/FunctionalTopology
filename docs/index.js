533a0f37bb75ef1c9caead02f83289eac7d61e53
Naruki-Ichihara <csna20005@g.nihon-u.ac.jp>
2021-07-11 10:23:13 +0000

    change index.js

--- docs/index.js
+++ docs/index.js
@@ -1,13 +1,1 @@
-import 'vtk.js/Sources/favicon';
-
-import macro from 'vtk.js/Sources/macro';
-import DataAccessHelper from 'vtk.js/Sources/IO/Core/DataAccessHelper';
-import HttpDataAccessHelper from 'vtk.js/Sources/IO/Core/DataAccessHelper/HttpDataAccessHelper';
-import vtkFullScreenRenderWindow from 'vtk.js/Sources/Rendering/Misc/FullScreenRenderWindow';
-import vtkHttpSceneLoader from 'vtk.js/Sources/IO/Core/HttpSceneLoader';
-import vtkURLExtract from 'vtk.js/Sources/Common/Core/URLExtract';
-
-import controlWidget from './SceneExplorerWidget';
-import style from './SceneExplorer.module.css';
-
 console.log("index.js: loaded");
